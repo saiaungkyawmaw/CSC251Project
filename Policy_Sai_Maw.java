8dff2eda7ea765cb42b9a2396befca15b16c7424
saiaungkyawmaw <52806006+saiaungkyawmaw@users.noreply.github.com>
2022-09-06 19:21:41 -0400

    debug and touch-up

--- Policy_Sai_Maw.java
+++ Policy_Sai_Maw.java
@@ -7,9 +7,9 @@ public class Policy_Sai_Maw
 {
    //fields to store relevant primary data
    private String pHolderFirstName,pHolderLastName;
-   private String pHolderSmokingStatus;
+   private char pHolderSmokingStatus;
    private int pHolderAge = 0;
-   private double pHolderWeightInLb; pHolderHeightInInch;
+   private double pHolderWeightInLb, pHolderHeightInInch;
    private String policyNum, providerName;
    
    //no-arg constructor
@@ -22,10 +22,10 @@ public class Policy_Sai_Maw
       pHolderLastName = "Doe";
       policyNum = "abcd1234";
       providerName = "Omnicorp";
-      pHolderSmokingStatus = "24/7";
+      pHolderSmokingStatus = 'y';
    }
    /**
-      parameter included constructor method
+      plug-in constructor method
       @param fName policy holder first name
       @param lName policy holder last name
       @param ss    policy holder smoking status
@@ -35,7 +35,7 @@ public class Policy_Sai_Maw
       @param pNum  policy number
       @param pName provider name
    */
-   public Policy_Sai_Maw(String fName, String lName, String ss, int age,
+   public Policy_Sai_Maw(String fName, String lName, char ss, int age,
                  double lb,double inch,String pNum, String pName)
    {
       pHolderFirstName = fName;
@@ -66,24 +66,122 @@ public class Policy_Sai_Maw
       @param inch policyholder height in inch
       @return premium
    */
-   public double premium(int age, String ss, double lb, double inch)
+   public double premium(int age, char ss, double lb, double inch)
    {
       final double baseFees = 600;
-      double premium = 0;
+      final double agePremium = 75;
+      final double smokerPremium = 100;
       double BMI = (lb * 703 ) / (inch * inch);
+      double premium = 0;
       
-      if ( age > 50 && ss && BMI > 35)
-      {
-         premium = baseFees + 75 + 100 + (( BMI – 35 ) * 20);
-         return premium;
-      }
-      else if ( age < 50 && ss && BMI > 35)
-      {
-         premium = baseFees + 100 + (( BMI – 35 ) * 20);
-      }
-
-   
+      if ( BMI > 35)
+         {
+            if ( age > 50 )
+               {
+                  if ( ss == 'y' )
+                     { premium = baseFees + agePremium + smokerPremium + (( BMI - 35 ) * 20); }
+                  else
+                     { premium = baseFees + agePremium + (( BMI - 35 ) * 20); }
+               }
+             else 
+               {
+                  if ( ss == 'y' )
+                     { premium = baseFees + smokerPremium + (( BMI - 35 ) * 20); }
+                  else
+                     { premium = baseFees + (( BMI - 35 ) * 20); }
+ 
+               } 
+         }
+       else
+         {
+           if ( age > 50 )
+               {
+                  if ( ss == 'y' )
+                     { premium = baseFees + agePremium + smokerPremium ; }
+                  else
+                     { premium = baseFees + agePremium ; }
+               }
+             else 
+               {
+                  if ( ss == 'y' )
+                     { premium = baseFees + smokerPremium ; }
+                  else
+                     { premium = baseFees ; }
+ 
+               }  
+         }
+         
+         return premium;   
    }
-      
+   /**
+      accessor methods for class 
+   */
+   public String getPolicyNum()
+   {
+      return policyNum;
+   }
+   public String getProviderName()
+   {
+      return providerName;
+   }
+   public String getPolicyholderFirstName()
+   {
+      return pHolderFirstName;
+   }
+   public String getPolicyholderLastName()
+   {
+      return pHolderLastName;
+   }
+   public int getPolicyholderAge()
+   {
+      return pHolderAge;
+   }
+   public double getPolicyholderWeight()
+   {
+      return pHolderWeightInLb;
+   }
+   public double getPolicyholderHeight()
+   {
+      return pHolderHeightInInch;
+   }
+   public char getPolicyholderSmokingStatus()
+   {
+      return pHolderSmokingStatus;
+   }
    
+   /**
+      mutator methods for class 
+   */
+   public void setPolicyNum(String pNum)
+   {
+      policyNum = pNum;
+   }
+   public void setProviderName(String proName)
+   {
+      providerName = proName;
+   }
+   public void setPolicyholderFirstName(String fName)
+   {
+     pHolderFirstName = fName;
+   }
+   public void setPolicyholderLastName(String lName)
+   {
+      pHolderLastName = lName;
+   }
+   public void setPolicyholderAge(int age)
+   {
+      pHolderAge = age;
+   }
+   public void setPolicyholderWeight(double weight)
+   {
+      pHolderWeightInLb = weight;
+   }
+   public void setPolicyholderHeight(double height)
+   {
+      pHolderHeightInInch = height;
+   }
+   public void setPolicyholderSmokingStatus(char ss)
+   {
+      pHolderSmokingStatus = ss;
+   }
 }
