687f5ce6e7331fa6d8e867304780a2efe97a35eb
saiaungkyawmaw <52806006+saiaungkyawmaw@users.noreply.github.com>
2022-10-09 14:32:14 -0400

    New features and correction
    New Features
    - read from file
    - count smoker/non-smoker
    
    Correction
    -field name, method, decision structure

--- Policy.java
+++ Policy.java
@@ -6,26 +6,26 @@
 public class Policy
 {
    //fields to store relevant primary data
-   private String pHolderFirstName;
-   private String pHolderLastName;
-   private String pHolderSmokingStatus;
-   private int pHolderAge = 0;
-   private double pHolderWeightInLb;
-   private double pHolderHeightInInch;
+   private String firstName;
+   private String lastName;
+   private String ss; //smoking status
+   private int age = 0;
+   private double weight;
+   private double height;
    private String policyNum;
    private String providerName;
    
    //no-arg constructor
    public Policy()
    {
-      pHolderAge = 0;
-      pHolderWeightInLb = 0;
-      pHolderHeightInInch = 0;
-      pHolderFirstName = "John/Jane";
-      pHolderLastName = "Doe";
+      age = 0;
+      weight = 0;
+      height = 0;
+      firstName = "John/Jane";
+      lastName = "Doe";
       policyNum = "abcd1234";
       providerName = "Omnicorp";
-      pHolderSmokingStatus = "";
+      ss = ""; //smoking status
    }
    /**
       plug-in constructor method
@@ -38,28 +38,26 @@ public class Policy
       @param pNum  policy number
       @param pName provider name
    */
-   public Policy(String fName, String lName, String ss, int age,
+   public Policy(String fName, String lName, String SS, int age,
                  double lb,double inch,String pNum, String pName)
    {
-      pHolderFirstName = fName;
-      pHolderLastName = lName;
-      pHolderSmokingStatus = ss;
-      pHolderAge = age;
-      pHolderWeightInLb = lb;
-      pHolderHeightInInch = inch;
+      firstName = fName;
+      lastName = lName;
+      ss = SS;
+      age = age;
+      weight = lb;
+      height = inch;
       providerName = pName;
       policyNum = pNum;
       
    }
    /**
       this method calculates and returns BMI for policy holder
-      @param lb policyholder weight in lb
-      @param inch policyholder height in inch
       @return BMI
    */
-   public double BMI(double lb, double inch)
+   public double BMI()
    {
-      return ( (lb * 703 ) / (inch * inch) );
+      return ( (weight * 703 ) / (height * height) );
    }
    /**
       this method calculates and returns the policy premium
@@ -69,131 +67,168 @@ public class Policy
       @param inch policyholder height in inch
       @return premium
    */
-   public double premium(int age, String ss, double lb, double inch)
+   public double premium()
    {
-      final double baseFees = 600;
-      final double agePremium = 75;
-      final double smokerPremium = 100;
-      double BMI = (lb * 703 ) / (inch * inch);
+      final double BASE_FEES = 600;
+      final double AGE_PREMIUM = 75;
+      final double SMOKER_PREMIUM = 100;
+      double BMI = (weight * 703 ) / (height * height);
+      double bmiFees = (BMI - 35 ) * 20;
       double premium = 0;
-      //for policyholder < BMI - 35
+      //policy holder over 35 BMI
       if ( BMI > 35)
-         {  
-            //for policy holder over the age of 50
-            if ( age > 50 )
-               {
-                  //check if smoker or not
-                  if ( ss.equalsIgnoreCase("smoker") )
-                     { premium = baseFees + agePremium + smokerPremium + (( BMI - 35 ) * 20); }
-                  else
-                     { premium = baseFees + agePremium + (( BMI - 35 ) * 20); }
-               }
-             //for policyholder = or under the age of 50
-             else 
-               {
-                  //check if smoker or not
-                  if ( ss.equalsIgnoreCase("smoker") )
-                     { premium = baseFees + smokerPremium + (( BMI - 35 ) * 20); }
-                  else
-                     { premium = baseFees + (( BMI - 35 ) * 20); }
- 
-               } 
+         {
+            if ( age > 50 && ss.equalsIgnoreCase("smoker")  )
+               { premium = BASE_FEES + AGE_PREMIUM + SMOKER_PREMIUM + bmiFees; }
+            else if ( age > 50 && ss.equalsIgnoreCase("non-smoker"))
+               { premium = BASE_FEES + AGE_PREMIUM + bmiFees; }
+            else if ( age <= 50 && ss.equalsIgnoreCase("smoker"))
+               { premium = BASE_FEES + SMOKER_PREMIUM + bmiFees;} 
+            else
+               { premium = BASE_FEES + bmiFees; }
          }
-       //for policyholder = or > BMI - 35
+       //policy holder under and equal to 35 BMI
        else
          {
-           //for policyholder over the age of 50
-           if ( age > 50 )
-               {
-                  //check if smoker or not
-                  if ( ss.equalsIgnoreCase("smoker") )
-                     { premium = baseFees + agePremium + smokerPremium ; }
-                  else
-                     { premium = baseFees + agePremium ; }
-               }
-             //for policyholder = or under the age of 50
-             else 
-               {
-                  //check if smoker or not
-                  if ( ss.equalsIgnoreCase("smoker") )
-                     { premium = baseFees + smokerPremium ; }
-                  else
-                     { premium = baseFees ; }
+           if ( age > 50 && ss.equalsIgnoreCase("smoker") )
+               { premium = BASE_FEES + AGE_PREMIUM + SMOKER_PREMIUM ; }
+           else if ( age > 50 && ss.equalsIgnoreCase("non-smoker"))
+               { premium = BASE_FEES + AGE_PREMIUM ; }
+           else if ( age <= 50 && ss.equalsIgnoreCase("smoker") )
+               { premium = BASE_FEES + SMOKER_PREMIUM ; }
+           else
+               { premium = BASE_FEES ; }
  
-               }  
-         }
-         
+          }  
          return premium;   
    }
    /**
-      accessor methods for class 
+   this method returns the policy number
+   @return policy number 
    */
    public String getPolicyNum()
    {
       return policyNum;
    }
+   /**
+   this method returns the provider number
+   @return provider number 
+   */
    public String getProviderName()
    {
       return providerName;
    }
+   /**
+   this method returns the first name
+   @return policy holder first name 
+   */
    public String getPolicyholderFirstName()
    {
-      return pHolderFirstName;
+      return firstName;
    }
+   /**
+   this method returns policy holder last name
+   @return policy holder last name
+   */
    public String getPolicyholderLastName()
    {
-      return pHolderLastName;
+      return lastName;
    }
+   /**
+   this method returns the age
+   @return age 
+   */
    public int getPolicyholderAge()
    {
-      return pHolderAge;
+      return age;
    }
+   /**
+   this method returns weight
+   @return weight
+   */
    public double getPolicyholderWeight()
    {
-      return pHolderWeightInLb;
+      return weight;
    }
+   /**
+   this method returns height
+   @return height
+   */
    public double getPolicyholderHeight()
    {
-      return pHolderHeightInInch;
+      return height;
    }
+   /**
+   this method returns smoking status
+   @return smoking status
+   */
    public String getPolicyholderSmokingStatus()
    {
-      return pHolderSmokingStatus;
+      return ss;
    }
    
    /**
-      mutator methods for class 
+   this method sets the policy number
+   @param pNum policy number 
    */
    public void setPolicyNum(String pNum)
    {
       policyNum = pNum;
    }
+   /**
+   this method sets the provider name
+   @param proName provider name 
+   */
    public void setProviderName(String proName)
    {
       providerName = proName;
    }
+   /**
+   this method sets the first name
+   @param fName first name
+   */
    public void setPolicyholderFirstName(String fName)
    {
-     pHolderFirstName = fName;
+     firstName = fName;
    }
+   /**
+   this method sets the last name
+   @param lName last name
+   */
    public void setPolicyholderLastName(String lName)
    {
-      pHolderLastName = lName;
+      lastName = lName;
    }
+   /**
+   this method sets the age
+   @param age policyholder age 
+   */
    public void setPolicyholderAge(int age)
    {
-      pHolderAge = age;
+      age = age;
    }
+   /**
+   this method sets the weight
+   @param weight weight 
+   */
    public void setPolicyholderWeight(double weight)
    {
-      pHolderWeightInLb = weight;
+      weight = weight;
    }
+   /**
+   this method sets the height
+   @param height policyholder height
+   */
    public void setPolicyholderHeight(double height)
    {
-      pHolderHeightInInch = height;
+      height = height;
    }
+   /**
+   this method sets the smoking status
+   @param ss smoking status 
+   */
    public void setPolicyholderSmokingStatus(String ss)
    {
-      pHolderSmokingStatus = ss;
+      ss = ss;
    }
 }
--- Policy_Demo.java
+++ Policy_Demo.java
@@ -2,46 +2,109 @@
    policy class demo___
 */
 
-import java.util.Scanner; //scanner class to read input
+import java.util.*; //scanner class to read input
+import java.io.*;
 
 public class Policy_Demo
 {
-   public static void main(String[] args)
+   public static void main(String[] args)throws IOException
    {
-      //variables to hold input
-      String pHolderFirstName,pHolderLastName;
-      String pHolderSmokingStatus;
-      int pHolderAge = 0;
-      double pHolderWeight, pHolderHeight;
+      //variables to hold individual object info
+      String firstName,lastName;
+      String smokingStatus;
+      int age = 0;
+      double weight, height;
       String policyNum, providerName;
+      //objet list
+      ArrayList<Policy> policyList = new ArrayList<Policy>();
+      //input file name
+      File file = new File("PolicyInformation.txt");
+      //smokers count
+      int smoker = 0;
       
-      //Scanner object to read input
-      Scanner keyboard = new Scanner(System.in);
+      //Scanner object to read text
+      Scanner inputFile = new Scanner(file);
       
-      //get input from user
+      //read line and create objects
+      while(inputFile.hasNextLine())
+      {
+         policyNum        = inputFile.next();
+         providerName     = inputFile.next();
+         firstName        = inputFile.next();
+         lastName         = inputFile.next();
+         age              = inputFile.nextInt();
+         smokingStatus    = inputFile.next();
+         height           = inputFile.nextDouble();
+         weight           = inputFile.nextDouble();
+         
+         policyList.add(new Policy(firstName,lastName,smokingStatus,age,weight,height,providerName,policyNum));
+         
+         if (smokingStatus.equalsIgnoreCase("smoker"))  { smoker ++;}   
+      }
+
+      for (int i = 0; i < policyList.size(); i++)
+      {
+         Policy policy = policyList.get(i);
+         printOut(policy);
+      }
+      
+      System.out.printf("The number of policies with a smoker is: %d\n",smoker); 
+      System.out.printf("The number of policies with a non-smoker is: %d\n", policyList.size() - smoker);  
+   }
+   /**
+   this method prints out detail info from the the policyholder list
+   and return the smokers count
+   @param poicyholder list
+   @return smokers count
+   */
+   public static void printOut(Policy obj)
+   {
+
+      System.out.printf("Policy Number: %s\n",obj.getPolicyNum());
+      System.out.printf("Provider Name: %s\n",obj.getProviderName());
+      System.out.printf("Policyholder's First Name: %s\n",obj.getPolicyholderFirstName());
+      System.out.printf("Policyholder's Last Name: %s\n",obj.getPolicyholderLastName());
+      System.out.printf("Policyholder's Age : %d\n",obj.getPolicyholderAge());
+      System.out.printf("Policyholder's Smoking Status: %s\n",obj.getPolicyholderSmokingStatus());
+      System.out.printf("Policyholder's Height: %,.2f inches\n",obj.getPolicyholderHeight());
+      System.out.printf("Policyholder's Weight: %,.2f lb\n",obj.getPolicyholderWeight());
+      System.out.printf("Policyholder's BMI: %,.2f\n",obj.BMI());
+      System.out.printf("Policy Price: $%,.2f \n",obj.premium());
+
+   }
+}
+
+/**
+input prompt
+//get input from user
       //set policy number
       System.out.print("Please enter the Policy Number: ");
       policyNum = keyboard.nextLine();
+      policy1.setPolicyNum(policyNum);
       System.out.println();
       
       //set provider name
       System.out.print("Please enter the Provider Name: ");
       providerName = keyboard.nextLine();
+      policy1.setProviderName(providerName);
       System.out.println();
       
       //set policyholder's first name
       System.out.print("Please enter the Policyholder's First Name: ");
       pHolderFirstName = keyboard.nextLine();
+      policy1.setPolicyholderFirstName(pHolderFirstName);
       System.out.println();
       
       //set policyholder's last name
       System.out.print("Please enter the Policyholder's Last Name: ");
       pHolderLastName = keyboard.nextLine();
+      policy1.setPolicyholderLastName(pHolderLastName);
       System.out.println();
       
       //set policyholder's age 
       System.out.print("Please enter the Policyholder's Age: ");
       pHolderAge = keyboard.nextInt();
+      policy1.setPolicyholderAge(pHolderAge);
       System.out.println();
       
       //buffer for next data entry
@@ -50,33 +113,18 @@ public class Policy_Demo
       //set policyholder's smoking status
       System.out.print("Please enter the Policyholder's Smoking Status (smoker/non-smoker): ");
       pHolderSmokingStatus = keyboard.nextLine();
+      policy1.setPolicyholderSmokingStatus(pHolderSmokingStatus);
       System.out.println(); 
       
       //set policyholder's height
       System.out.print("Please enter the Policyholder's Height (in inches): ");
       pHolderHeight = keyboard.nextDouble();
+      policy1.setPolicyholderHeight(pHolderHeight);
       System.out.println();
       
       //set policyholder's weight
       System.out.print("Please enter the Policyholder's Weight (in pounds): ");
       pHolderWeight = keyboard.nextDouble();
+      policy1.setPolicyholderWeight(pHolderWeight);
       System.out.println();
-      
-      //policy object to store data
-      Policy policy1 = new Policy(pHolderFirstName, pHolderLastName, pHolderSmokingStatus, pHolderAge, 
-                                  pHolderWeight, pHolderHeight, policyNum, providerName);
-      
-      //print out relevant statements
-      System.out.printf("Policy Number: %s\n",policy1.getPolicyNum());
-      System.out.printf("Provider Name: %s\n",policy1.getProviderName());
-      System.out.printf("Policyholder's First Name: %s\n",policy1.getPolicyholderFirstName());
-      System.out.printf("Policyholder's Last Name: %s\n",policy1.getPolicyholderLastName());
-      System.out.printf("Policyholder's Age : %d\n",policy1.getPolicyholderAge());
-      System.out.printf("Policyholder's Smoking Status: %s\n",policy1.getPolicyholderSmokingStatus());
-      System.out.printf("Policyholder's Height: %,.2f inches\n",policy1.getPolicyholderHeight());
-      System.out.printf("Policyholder's Weight: %,.2f lb\n",policy1.getPolicyholderWeight());
-      System.out.printf("Policyholder's BMI: %,.2f\n",policy1.BMI(pHolderWeight,pHolderHeight));
-      System.out.printf("Policy Price: $%,.2f \n",policy1.premium(pHolderAge, pHolderSmokingStatus, pHolderWeight, pHolderHeight));
-      
-   }
-}
+*/
